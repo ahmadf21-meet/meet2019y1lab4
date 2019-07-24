4079bdd82447c959777231625d35d5015c78dce2
ahmadf21-meet <ahmadf21@meet.mit.edu>
2019-07-24 10:00:21 -0400

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,10 @@
+fruit=input('what kind of fruit do you have?')
+if fruit=='apple'or fruit=='apples':
+    print('apples belong in bin 1')
+elif fruit=='banana'or fruit=='bananas':
+    print('bananas belong in bin 3')
+elif fruit=='oranges'or fruit=='orange':
+    print('oranges belong in bin 2')
+else:
+    print("Error! I don't recognize what fruit this is ")
+    
